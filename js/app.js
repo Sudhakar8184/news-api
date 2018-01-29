706d89554b3b0c6e5625954925171ce3880a94c8
Sudhakar8184 <chintareddy.sudhakarreddy@gmail.com>
2018-01-29 23:46:19 +0530

    app.js update

--- js/app.js
+++ js/app.js
@@ -1,182 +1,154 @@
-
-<<<<<<< HEAD
-
-     document.getElementById("search").addEventListener("click", news_get);
+document.getElementById("search").addEventListener("click", news_get);
         function news_get(){
+            var first = document.getElementById('first');
+            first.innerHTML="";
             var search_data = document.forms["myForm"]["search-data"].value;
-            var news_channel = document.forms["myForm"]["news-channel"].value;
+            //var news_channel = document.forms["myForm"]["news-channel"].value;
+            var a=[];
+          for(i=0;i<8;i++){
+           if(document.getElementById("id"+i).checked){
+          a.push(document.getElementById("id"+i).value);
+          }
+          }
+          console.log(a);
+          a.forEach((Array1)=>{
+              console.log(Array1)
+          })
             console.log(search_data);
-            console.log(news_channel);
-            fetch(`https://newsapi.org/v2/everything?q=${search_data}&sources=${news_channel}&apiKey=3449fa02267b40518f1422b315334332`)
-=======
-var buttons=document.getElementsByClassName("button")
-var buttonsCount = buttons.length;
-
-for (var i = 0; i <= buttonsCount; i += 1) {
-    buttons[i].onclick = function(e) {
-    // alert(this.id)
-    var first = document.getElementById('first');
-    first.innerHTML="";
-   (function(news){
-        fetch(`https://newsapi.org/v2/top-headlines?sources=${news}&apiKey=3449fa02267b40518f1422b315334332`)
->>>>>>> 334a40e0cf3b14a290a6b5ec5adf9173a5473b13
-        .then((res)=>{
-            return res.json()
-        }).then((data)=>{
-            for (var i = 0; i < data.totalResults; i++) {
-
-                var block= document.createElement('div');
-                var block1= document.createElement('div');
-                var block2= document.createElement('div');
-                var block3= document.createElement('div');
-                block.setAttribute('id','head-news')
-                block3.setAttribute('id','clear')
-                block2.setAttribute('id','head-img')
-                block1.setAttribute('id','head-title-des')
-
-                var title = document.createElement('h3');
-                title.setAttribute('id','head-title');
-
-<<<<<<< HEAD
-
-
-    var buttons=document.getElementsByClassName("button")
-    var buttonsCount = buttons.length;
-    for (let i = 0; i <= buttonsCount; i += 1) {
-        buttons[i].onclick = function(e) {
-        
-           gettext(this.id);
-       
-        };
+          //  console.log(news_channel);
+            if(search_data === '' || a == ''){
+                 alert("please  select the channel and enter the data before search");
+            }else{
+             a.forEach((Array_channel)=>{
+                fetch(`https://newsapi.org/v2/everything?q=${search_data}&sources=${Array_channel}&apiKey=3449fa02267b40518f1422b315334332`)
+                .then((res)=>{
+                    return res.json();
+                }).then((data)=>{
+                    if(data.articles == ""){
+                        let head = document.getElementById('first');
+                        head.innerHTML=`<center><h3>NO data is present on this search of <b>${search_data}</b>on this <b>${Array_channel}</b> </h3></center>`;
+                    }else if(data.status === "error"){
+                        let block=document.createElement('p')
+                        let head = document.getElementById('new-pages');
+                        p.innerHTML="NO data is present on this search ";
+                        
+                    }else{
+                    for(let j = 0; j < data.totalResults; j++) {
+                        
+                        if(data.articles[j].urlToImage === null || data.articles[j].urlToImage == "undefined"){
+                            console.log(data.articles[j].urlToImage);
+                           continue;
+                        }else{
+                            let markup =`
+                            <div class="row" >
+                            <div class="col-md-3">
+                            <img src="${data.articles[j].urlToImage}" width="200px" height="150px" >
+                            </div>
+                            <div class="col-md-9">
+                            <h3>${data.articles[j].source.name}</h3>
+                            <a href="${data.articles[j].url}"><h3>${data.articles[j].title}</h3> </a>
+                               <p>${data.articles[j].description}</p>
+                          </div>
+                        </div>`;
+                        let block= document.createElement('div');
+                        block.setAttribute('id','head-news');
+                        block.innerHTML=markup;
+                        const head =document.getElementById('first');
+                        head.appendChild(block);
+                        } 
+                    } 
+                    }
+                }).catch((e)=>{
+                    console.log(e);
+                })
+             })
+       }
     }
- //document.getElementsByClassName("button").addEventListener("click", gettext;
-        function gettext(news) {
-              
+
+     
+	   
+    var buttons=document.getElementsByClassName("button");
+    
+    var buttonsCount = buttons.length;
+    for (let i = 0; i < buttonsCount; i += 1) {
+        buttons[i].addEventListener("click", function() {
+            var id= this.id;
+            if(id=="sports"||id=="technology"||id=="entertainment"){
+                getnav(this.id)
+            }else{
+                gettext(this.id);
+            }
+           
+           var first = document.getElementById('first');
+             first.innerHTML="";
+        });
+    
+    }
+         function gettext(news) {     
             fetch(`https://newsapi.org/v2/top-headlines?sources=${news}&apiKey=3449fa02267b40518f1422b315334332`)
         .then((res)=>{
             return res.json()
         }).then((data)=>{
-           
+           // head.replaceChild(block,head)
                  for (let j = 0; j < data.totalResults; j++) {
-                   
+                    if(data.articles[j].urlToImage === null || data.articles[j].urlToImage == "undefined"){
+                       continue
+                    }
                     let markup =`
-                    <div class="row">
+                    <div class="row " >
                     <div class="col-md-3">
-                    <img src="${data.articles[j].urlToImage}" width="150" height="150px" >
+                    <img src="${data.articles[j].urlToImage}" width="200px" height="150px" >
                     </div>
                     <div class="col-md-9">
-                    <a href=""   ><h3>${data.articles[j].title}</h3> </a>
+                    
+                    <a href="${data.articles[j].url}"><h3>${data.articles[j].title}</h3> </a>
                        <p>${data.articles[j].description}</p>
                   </div>
                 </div>`;
                 let block= document.createElement('div');
                 block.setAttribute('id','head-news');
                 block.innerHTML=markup;
-                let head =document.getElementById('first');
-                head.appendChild(block);  
-=======
-                var description = document.createElement('p');
-                description.setAttribute('id','head-description');
-
-                var anchor = document.createElement('a');
-                anchor.setAttribute('id','head-anchor');
-                anchor.setAttribute('href',data.articles[i].url);
-
-                var img = document.createElement('img');
-                 img.setAttribute('src',data.articles[i].urlToImage);
-                 img.setAttribute('width','150px');
-                 img.setAttribute('height','140px');
-                 img.setAttribute('alt','image not available');
-
-                var head = document.getElementById('first');
-                title.innerHTML= data.articles[i].title;
-                description.innerHTML= data.articles[i].description;
-                block2.appendChild(img);
-                anchor.appendChild(title);
-                block1.appendChild(anchor);
-                block1.appendChild(description);
-                block.appendChild(block2);
-                block.appendChild(block1);
-                block.appendChild(block3);
+                const head =document.getElementById('first');
                 head.appendChild(block);
-
->>>>>>> 334a40e0cf3b14a290a6b5ec5adf9173a5473b13
-            }
+            
+        }
+            
+        }).catch((e)=>{
+            console.log(e);
         })
-<<<<<<< HEAD
        }
-	   
-	   
-	   
-	   
-	   
-=======
-    })(this.id);
-    };
-}
 
 
+            function getnav(news) {
+               fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${news}&apiKey=3449fa02267b40518f1422b315334332`)
+           .then((res)=>{
+               return res.json()
+           }).then((data)=>{
+              // head.replaceChild(block,head)
+                    for (let j = 0; j < data.totalResults; j++) {
+                       if(data.articles[j].urlToImage === null || data.articles[j].urlToImage == "{{pageImage}}" ||data.articles[j].urlToImage == "undefined"){
+                          continue
+                       }
+                       let markup =`
+                       <div class="row ">
+                       <div class="col-md-3">
+                       <img src="${data.articles[j].urlToImage}" width="200px" height="150px" >
+                       </div>
+                       <div class="col-md-9">
+                       
+                       <a href="${data.articles[j].url}"><h3>${data.articles[j].title}</h3> </a>
+                          <p>${data.articles[j].description}</p>
+                     </div>
+                   </div>`;
+                   let block= document.createElement('div');
+                   block.setAttribute('id','head-news');
+                   block.innerHTML=markup;
+                   const head =document.getElementById('first');
+                   head.appendChild(block);
+               
+           }   
+           }).catch((e)=>{
+            console.log(e);
+        })
+          }
 
-    //  document.getElementById("search").addEventListener("click", news_get);
-    //     function news_get(){
-    //         var search_data = document.forms["myForm"]["search-data"].value;
-    //         var news_channel = document.forms["myForm"]["news-channel"].value;
-    //         console.log(search_data);
-    //         console.log(news_channel);
-    //         fetch(`https://newsapi.org/v2/everything?q=${search_data}&sources=${news_channel}&apiKey=3449fa02267b40518f1422b315334332`)
-    //     .then((res)=>{
-    //         return res.json()
-    //     }).then((data)=>{
-    //         if(data.articles[0]==""){
-    //             let head = document.getElementById('first');
-    //             let description = document.createElement('p');
-    //             description.innerHTML="NO data is present on this search";
-    //             head.appendChild(description);
-    //         }else{
-    //         for(let j = 0; j < data.totalResults; j++) {
-    //             let block= document.createElement('div');
-    //             let block1= document.createElement('div');
-    //             let block2= document.createElement('div');
-    //             let block3= document.createElement('div');
-    //             block.setAttribute('id','head-news')
-    //             block3.setAttribute('id','clear')
-    //             block2.setAttribute('id','head-img')
-    //             block1.setAttribute('id','head-title-des')
-
-    //             let title = document.createElement('h3');
-    //             title.setAttribute('id','head-title');
-
-    //             let description = document.createElement('p');
-    //             description.setAttribute('id','head-description');
-
-
-    //             let anchor = document.createElement('a');
-    //             anchor.setAttribute('id','head-anchor');
-    //             anchor.setAttribute('href',data.articles[j].url);
-
-    //             let img = document.createElement('img');
-    //              img.setAttribute('src',data.articles[j].urlToImage);
-    //              img.setAttribute('width','150px');
-    //              img.setAttribute('height','140px');
-    //              img.setAttribute('alt','this is image');
-
-    //             let head = document.getElementById('first');
-    //             title.innerHTML= data.articles[j].title;
-    //             description.innerHTML= data.articles[j].description;
-    //             block2.appendChild(img);
-    //             anchor.appendChild(title);
-    //             block1.appendChild(anchor);
-    //             block1.appendChild(description);
-    //             block.appendChild(block2);
-    //             block.appendChild(block1);
-    //             block.appendChild(block3);
-    //             head.appendChild(block);
-    //         }
-    //         }
-    //     })
-    //    }
-
-
-
-
->>>>>>> 334a40e0cf3b14a290a6b5ec5adf9173a5473b13

7d480f7ce4fc68f60acaa2d9390cb699fa7dcfc9
Sudhakar8184 <chintareddy.sudhakarreddy@gmail.com>
2018-01-30 14:38:36 +0530

    new update

--- js/app.js
+++ js/app.js
@@ -1,58 +1,62 @@
-document.getElementById('search').addEventListener('click', news_get);
-function news_get() {
+document.getElementById('search').addEventListener('click', newsGet);
+function newsGet() {
   const first = document.getElementById('first');
   first.innerHTML = '';
-  const search_data = document.forms.myForm['search-data'].value;
+  const searchData = document.forms.myForm['search-data'].value;
   // var news_channel = document.forms["myForm"]["news-channel"].value;
-  const a = [];
-  for (i = 0; i < 8; i++) {
+  const arr = [];
+  for (let i = 0; i < 8; i += 1) {
     if (document.getElementById(`id${i}`).checked) {
-      a.push(document.getElementById(`id${i}`).value);
+      arr.push(document.getElementById(`id${i}`).value);
     }
   }
-  console.log(a);
-  a.forEach((Array1) => {
-    console.log(Array1);
-  });
-  console.log(search_data);
+  //   console.log(a);
+  //   a.forEach((Array1)=>{
+  //       console.log(Array1)
+  //   })
+  //     console.log(searchData);
   //  console.log(news_channel);
-  if (search_data === '' || a == '') {
+  if (searchData === '' || arr == '') {
     alert('please  select the channel and enter the data before search');
   } else {
-    a.forEach((Array_channel) => {
-      fetch(`https://newsapi.org/v2/everything?q=${search_data}&sources=${Array_channel}&apiKey=3449fa02267b40518f1422b315334332`)
-        .then(res => res.json()).then((data) => {
+    arr.forEach((arrayChannel) => {
+      fetch(`https://newsapi.org/v2/everything?q=${searchData}&sources=${arrayChannel}&apiKey=3449fa02267b40518f1422b315334332`)
+        .then(res => res.json())
+        .then((data) => {
           if (data.articles == '') {
+            let head = document.getElementById('first');
+            let block = document.createElement('div');
+            block.innerHTML = `<center><h3>NO data is present on this search of <b>${searchData}</b> on this <b>${arrayChannel}</b> channel </h3></center>`;
+            head.appendChild(block);
+          } else if (data.status == 'error') {
             const head = document.getElementById('first');
-            head.innerHTML = `<center><h3>NO data is present on this search of <b>${search_data}</b>on this <b>${Array_channel}</b> </h3></center>`;
-          } else if (data.status === 'error') {
-            const block = document.createElement('p');
-            const head = document.getElementById('new-pages');
-            p.innerHTML = 'NO data is present on this search ';
+            const block = document.createElement('div');
+            block.innerHTML = 'NO data is present on this search';
+            head.appendChild(block);
           } else {
-            for (let j = 0; j < data.totalResults; j++) {
-              if (data.articles[j].urlToImage === null || data.articles[j].urlToImage == 'undefined') {
+            for (let j = 0; j < data.totalResults; j += 1) {
+              if (data.articles[j].urlToImage === null || data.articles[j].urlToImage === undefined) {
                 console.log(data.articles[j].urlToImage);
-                continue;
               } else {
-                const markup = `
+                let markup = `
                             <div class="row" >
                             <div class="col-md-3">
-                            <img src="${data.articles[j].urlToImage}" width="200px" height="150px" >
+                            <img src="${data.articles[j].urlToImage}" id="head-img" width="230px" height="180px" >
                             </div>
                             <div class="col-md-9">
-                            <h3>${data.articles[j].source.name}</h3>
+                            <h4 id="head-chl" >${data.articles[j].source.name}</h4>
                             <a href="${data.articles[j].url}"><h3>${data.articles[j].title}</h3> </a>
-                               <p>${data.articles[j].description}</p>
+                               <p id="head-des">${data.articles[j].description}</p>
                           </div>
                         </div>`;
-                const block = document.createElement('div');
+                let block = document.createElement('div');
                 block.setAttribute('id', 'head-news');
                 block.innerHTML = markup;
-                const head = document.getElementById('first');
+                let head = document.getElementById('first');
                 head.appendChild(block);
               }
             }
+            
           }
         }).catch((e) => {
           console.log(e);
@@ -73,7 +77,6 @@ for (let i = 0; i < buttonsCount; i += 1) {
     } else {
       gettext(this.id);
     }
-
     const first = document.getElementById('first');
     first.innerHTML = '';
   });
@@ -83,53 +86,58 @@ function gettext(news) {
     .then(res => res.json()).then((data) => {
       // head.replaceChild(block,head)
       for (let j = 0; j < data.totalResults; j += 1) {
-        if (data.articles[j].urlToImage === null || data.articles[j].urlToImage == 'undefined') {
-          continue;
+        if (data.articles[j].urlToImage === null || data.articles[j].urlToImage === 'undefined') {
+          console.log(data.articles[j].urlToImage);
+        } else {
+          let markup = `
+          <div class="row " >
+          <div class="col-md-3">
+          <img src="${data.articles[j].urlToImage}" id="head-img" width="230px" height="180px" >
+          </div>
+          <div class="col-md-9">
+          
+          <a href="${data.articles[j].url}"><h3>${data.articles[j].title}</h3> </a>
+             <p id="head-des">${data.articles[j].description}</p>
+        </div>
+      </div>`;
+          let block = document.createElement('div');
+          block.setAttribute('id', 'head-news');
+          block.innerHTML = markup;
+          let head = document.getElementById('first');
+          head.appendChild(block);
         }
-        const markup = `
-                    <div class="row " >
-                        <div class="col-md-3">
-                            <img src="${data.articles[j].urlToImage}" width="200px" height="150px" >
-                        </div>
-                        <div class="col-md-9">
-                        <a href="${data.articles[j].url}"><h3>${data.articles[j].title}</h3> </a>
-                            <p>${data.articles[j].description}</p>
-                        </div>
-                    </div>`;
-        const block = document.createElement('div');
-        block.setAttribute('id', 'head-news');
-        block.innerHTML = markup;
-        const head = document.getElementById('first');
-        head.appendChild(block);
       }
     }).catch((e) => {
       console.log(e);
     });
 }
 
+
 function getnav(news) {
   fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${news}&apiKey=3449fa02267b40518f1422b315334332`)
     .then(res => res.json()).then((data) => {
       // head.replaceChild(block,head)
-      for (let j = 0; j < data.totalResults; j++) {
-        if (data.articles[j].urlToImage === null || data.articles[j].urlToImage == '{{pageImage}}' || data.articles[j].urlToImage == 'undefined') {
-          continue;
-        }
-        const markup = `
+      for (let j = 0; j < data.totalResults; j += 1) {
+        if (data.articles[j].urlToImage === null || data.articles[j].urlToImage === '{{pageImage}}' || data.articles[j].urlToImage === 'undefined') {
+          console.log(data.articles[j].urlToImage);
+        } else {
+          let markup = `
                        <div class="row ">
-                            <div class="col-md-3">
-                                <img src="${data.articles[j].urlToImage}" width="200px" height="150px" >
-                            </div>
-                            <div class="col-md-9">
-                                <a href="${data.articles[j].url}"><h3>${data.articles[j].title}</h3> </a>
-                                    <p>${data.articles[j].description}</p>
-                            </div>
-                        </div>`;
-        const block = document.createElement('div');
-        block.setAttribute('id', 'head-news');
-        block.innerHTML = markup;
-        const head = document.getElementById('first');
-        head.appendChild(block);
+                       <div class="col-md-3">
+                       <img src="${data.articles[j].urlToImage}" id="head-img" width="230px" height="180px" >
+                       </div>
+                       <div class="col-md-9">
+                       
+                       <a href="${data.articles[j].url}"><h3>${data.articles[j].title}</h3> </a>
+                          <p id="head-des">${data.articles[j].description}</p>
+                     </div>
+                   </div>`;
+          let block = document.createElement('div');
+          block.setAttribute('id', 'head-news');
+          block.innerHTML = markup;
+          let head = document.getElementById('first');
+          head.appendChild(block);
+        }
       }
     }).catch((e) => {
       console.log(e);
